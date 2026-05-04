b752668a3a941bde1f72c0ba9909ef19187d58c4
Andrey Polyakov <andrey@polyakov.im>
2026-05-04 17:05:58 +0300

    chore: initial commit

---
+++ generate.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by: Andrey Polyakov (andrey@polyakov.im)
+ */
+
+return [
+    'openapi-file' => dirname(__DIR__, 2) . '/dist/pecom.json',
+    'namespace' => 'glook\PecomSdk',
+    'directory' => __DIR__ . '/src',
+];
